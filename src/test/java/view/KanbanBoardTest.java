99426b4e7f752baae66b6ce1ac786b2003619c58
Meewnicorn <nanou.beyazian@gmail.com>
2019-11-09 15:41:31 +0000

    Add Test for KanbanBoard using Automaton

---
+++ src/test/java/view/KanbanBoardTest.java
@@ -0,0 +1,15 @@
+package view;
+import model.KanbanBoard;
+import org.junit.Test;
+import com.athaydes.automaton.Swinger;
+
+
+public class KanbanBoardTest {
+
+    @Test
+    public void testApp(){
+        new KanbanBoard();
+        Swinger swinger = Swinger.forSwingWindow();
+        swinger.pause(5000);
+    }
+}
