2da631985aa37d25fd4bd71642f6a94c60a2e040
pcscutaru <petrascutaru@Hotmail.com>
2019-11-17 01:03:55 +0000

    Add comments

--- src/main/java/model/Change.java
+++ src/main/java/model/Change.java
@@ -1,22 +1,38 @@
 package model;
 
+/**
+ * This class stores the attributes a single Change object
+ * requires before it is used withing the change log.
+ */
+
 public class Change{
 
     private String objID;
     private ChangeType changeType;
     private String className;
 
+    /**
+     * Enums are public, static and final values, here specifying the type
+     * of a change which is possible.
+     */
+    enum ChangeType{
+        ADD, REMOVE, UPDATE
+    }
+
     public Change(ChangeType changeType, String objID, Class<?> classType) {
         this.changeType = changeType;
         this.objID = objID;
-        if (classType == BoardPanel.class) {
+        if (classType == BoardPanel.class) { //check for the class names passed TODO: add all types of objects that we use
             className = "BoardPanel";
         }
         //else if (cls == int.class) { ... }
     }
 
-    enum ChangeType{
-        ADD, REMOVE, UPDATE
-    }
+    //if change is add/update this will acc create a new obj with the relevant change
+    //public Object createObject(){
+        // if classname e.g. BoardPanel
+        // create new BoardPanel with stored values and return
+    //}
+
 
 }
--- src/main/java/model/ChangeLog.java
+++ src/main/java/model/ChangeLog.java
@@ -3,11 +3,19 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is responsible for the storing of changes in the state of a board.
+ */
+
 public class ChangeLog {
     private List<Change> changes = new ArrayList<Change>();
 
     public ChangeLog(){
 
+    }
+
+    public void addChange(Change change){
+        changes.add(change);
     }
 
 }
