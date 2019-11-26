e4ed1a667dc38abf6fa85db993d91e120828d1b8
Meewnicorn <jeanne.beyazian@gmail.com>
2019-11-26 13:05:20 +0000

    Add KanbanCard to KanbanCardButton

--- src/main/java/view/KanbanCardButton.java
+++ src/main/java/view/KanbanCardButton.java
@@ -3,12 +3,23 @@ package view;
 import javax.swing.*;
 
 public class KanbanCardButton extends JButton {
-    private String title;
-    private String description;
-    private int storyPoints;
+
+    private String cardTitle;
+    private KanbanCard card;
 
     public KanbanCardButton(String name, String description, int storyPoints) {
         super(name);
+        setBackground(new java.awt.Color(153, 240, 168));
+        cardTitle=name;
+        card = new KanbanCard(name, description, storyPoints);
+    }
 
+
+    public String getCardTitle() {
+        return cardTitle;
+    }
+
+    public KanbanCard getCard(){
+        return card;
     }
 }
