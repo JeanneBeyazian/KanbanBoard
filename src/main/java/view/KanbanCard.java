9b9c72111e622dfee4f84d7ca7273e2c2fc2860b
nathank1894 <55813157+nathank1894@users.noreply.github.com>
2019-11-26 12:18:04 +0000

    set up textArea and sizes

--- src/main/java/view/KanbanCard.java
+++ src/main/java/view/KanbanCard.java
@@ -3,6 +3,8 @@ package view;
 import annotations.ClassAnnotation;
 
 import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.*;
 
 /**
  * This class is responsible for the creation of kanban cards.
@@ -14,7 +16,7 @@ import javax.swing.*;
         creationDate = "13/11/2019",
         lastEdit = "22/11/2019"
 )
-public class KanbanCard extends JPanel {
+public class KanbanCard extends JFrame {
 
     // TODO : Give a unique ID to each card
     // TODO : Fix the group layout - or change to a new type of layout
@@ -23,27 +25,57 @@ public class KanbanCard extends JPanel {
     private String description;
     private int storyPoints;
 
+
     public KanbanCard(String name, String description, int storyPoints) {
 
-        /**JLabel title = new JLabel(name);
-        JLabel info = new JLabel(description);
-        JLabel points = new JLabel(String.valueOf(storyPoints));
 
-        GroupLayout layout = new GroupLayout(this);
-        this.setLayout(layout);
 
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
 
-        layout.setVerticalGroup(layout.createSequentialGroup()
-                        .addComponent(title)
-                        .addComponent(info)
-                        .addComponent(points)
-        );*/
+        JTextArea titleLabel = new JTextArea(name);
+        JTextArea info = new JTextArea(description);
+        JTextArea points = new JTextArea(String.valueOf(storyPoints));
+        JLabel emp = new JLabel("       ");
+        info.setRows(20);
+        info.setColumns(30);
+//        JButton delete = new JButton("delete");
+//        delete.setBounds(1000,10,100,100);
+        setSize(600, 600);
+        //setLayout(new FlowLayout());
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setSize(600,550);
+        add(panel);
+        JButton delete = new JButton("delete");
+        delete.setBounds(500,500,5,5);
+        panel.add(titleLabel);
+        panel.add(emp);
+        panel.add(info);
+        panel.add(points);
+        panel.add(delete);
 
-        add(new JLabel("This is a card"));
+//        panel.setLayout(null);
+//        panel.add(titleLabel);
+//        panel.add(delete);
+        setLayout(new FlowLayout());
+        //add(titleLabel);
+        //add(info);
+        //add(delete);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        //getContentPane().setBackground(Color.BLUE);
+
+        //add(new JLabel("This is a card"));
 
     }
 
+
+
+    public void setDescription(String newDes) {
+        description = newDes;
+    }
+
+    public void setPoint(int newPoint) {
+        storyPoints = newPoint;
+    }
 
 }
