c3a67f96ea48a8abc12d83e7bb8f5853596f6226
Meewnicorn <jeanne.beyazian@gmail.com>
2019-11-26 12:22:43 +0000

    Add arrayList for KanbanCard

--- src/main/java/view/KanbanColumn.java
+++ src/main/java/view/KanbanColumn.java
@@ -4,7 +4,6 @@ import annotations.ClassAnnotation;
 import controller.ColumnRole;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -17,19 +16,18 @@ import java.util.ArrayList;
 public class KanbanColumn extends JPanel {
 
     // TODO : give a unique ID to each column DONE!
-    // TODO : choose a proper size for column using constants below DONE!
-    // TODO (J) : the columnPane (scrollPane) should have one container in which the cards are added.
-    // TODO (J) : the container should keep a list of buttons pointing to cards
 
-    private String id;
+    private ArrayList<KanbanCard> cards;
+    private int id;
     private ColumnRole role;
     private JScrollPane columnPane;
     private int WIDTH;
     private int HEIGHT;
 
-    public KanbanColumn(String columnTitle, ColumnRole role) {
-
+    public KanbanColumn(String columnTitle, ColumnRole role, int id) {
+        cards = new ArrayList<KanbanCard>();
         this.role = role;
+        this.id = id;
         columnPane = new JScrollPane();
 
         //setSize(WIDTH,LENGTH);
@@ -37,6 +35,9 @@ public class KanbanColumn extends JPanel {
     }
   
     private void initialiseColumn(String nameIn) {
+
+        JLabel ID = new JLabel(String.valueOf(id));
+        add(ID);
 
         setBackground(new java.awt.Color(86, 164, 194));
 
@@ -49,12 +50,13 @@ public class KanbanColumn extends JPanel {
         add(Box.createVerticalGlue());
         add(columnPane);
 
-        addCard(new KanbanCard("Card one", "This is the first card", 10));
-        addCard(new KanbanCard("Card two", "This is the second card", 20));
+        addCard(new KanbanCardButton("Card one", "This is the first card", 10));
+        addCard(new KanbanCardButton("Card two", "This is the second card", 20));
     }
 
     private void addCard(KanbanCard card) {
         //card.setAlignmentX(Component.CENTER_ALIGNMENT);
+        cards.add(card);
         add(card);
         columnPane.add(card);
     }
@@ -74,10 +76,6 @@ public class KanbanColumn extends JPanel {
     
     private void setHeight(int inHeight) {
     	HEIGHT = inHeight;
-    }
-    private String generateId() {
-    	int idCounter = 0;
-    	return String.valueOf(idCounter++);
     }
     
 
