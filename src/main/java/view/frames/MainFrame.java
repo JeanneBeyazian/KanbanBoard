a2e0832eb488181816a92f19f685cb3d78197819
Meewnicorn <nanou.beyazian@gmail.com>
2019-12-12 11:59:06 +0000

    Add mainframe logo

--- src/main/java/view/frames/MainFrame.java
+++ src/main/java/view/frames/MainFrame.java
@@ -5,13 +5,13 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame {
+
     private JPanel container;
 
     public MainFrame() {
-        setTitle("KanbanBoard App");
-        setLocationRelativeTo(null);
-        //setLayout(new FlowLayout());
+        setTitle("Welcome to Kanban Board Indigo");
+        //setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         initialise();
         pack();
@@ -20,35 +20,39 @@ public class MainFrame extends JFrame{
     private void initialise() {
 
         container = new JPanel();
-        container.setPreferredSize(new Dimension(500, 500));
 
         JLabel welcomeLabel = new JLabel("Welcome to the Kanban Board Indigo App !");
 
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
+        //JPanel buttonPanel = new JPanel();
+        //buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
 
         JButton newBoard = new JButton("Create new board");
         newBoard.addActionListener(e-> new CreateFrame().setVisible(true));
-        buttonPanel.add(newBoard);
+        //buttonPanel.add(newBoard);
 
-        buttonPanel.add(Box.createRigidArea(new Dimension(20, 0)));
+        //buttonPanel.add(Box.createRigidArea(new Dimension(20, 0)));
 
         JButton open = new JButton("Open existing board");
         open.addActionListener(e-> new OpenFrame().setVisible(true));
-        buttonPanel.add(open);
+        //buttonPanel.add(open);
 
-        container.add(welcomeLabel, BorderLayout.NORTH);
-        container.add(buttonPanel, BorderLayout.CENTER);
+        container.add(new JLabel(new ImageIcon("src/images/kanban_logo.png")));
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        panel.add(welcomeLabel);
+        panel.add(newBoard);
+        panel.add(open);
+        container.add(new JSeparator());
+        container.add(panel);
         open.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
             }
         });
         add(container);
-//        add(makeContainerPanel());
 
-
-        //createNew.addActionListener(newProject());
+        pack();
 
         setResizable(true);
 
